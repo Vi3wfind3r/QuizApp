f6113b0b609f2ab573b7f76c7d8f6ed6678388f2
PROB8 <blackb8r@gmail.com>
2017-05-15 12:50:06 -0400

    second commit

--- app.js
+++ app.js
@@ -5,19 +5,48 @@ const quiz = {
       question: "What is the capital of Maine?",
       answer: ['Augusta', 'Bangor', 'Lake City', 'Waterville'],
       correctAnswer: 0
-    },
-    {
-      id: 1,
-      question: "What is the capital of Wyoming?",
-      answer: ['Cody', 'Laramie', 'Cheyenne', 'Yellowstone'],
-      correctAnswer: 2
     }
+    // {
+    //   id: 1,
+    //   question: "What is the capital of Wyoming?",
+    //   answer: ['Cody', 'Laramie', 'Cheyenne', 'Yellowstone'],
+    //   correctAnswer: 2
+    // }
   ],
 
   userAnswer: [],
   currentQuestion: null
 
 };
+
+//////////////////////////MOD FUNCTIONS//////////////////////////////////
+
+  function selectAnswer(answerIndex){
+
+    return quiz.userAnswer.push(answerIndex);
+  }
+
+console.log('quiz', selectAnswer(0), quiz.userAnswer);
+
+//   function nextQuestion(){
+//     to display the next question
+
+//     return 
+//   }
+// nextQuestion();
+// console.log(quiz);
+// selectAnswer(1);
+// console.log(quiz);
+// nextQuestion();
+// selectAnswer(0);
+// nextQuestion();
+// newQuiz();
+
+
+
+
+
+  // <li id="0">Augusta</li>
 
 
 
