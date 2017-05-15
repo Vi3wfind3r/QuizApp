ac19c3e0c21282a2bd8396abe9910ce39bd3b174
Jamie Wang <sephona@gmail.com>
2017-05-15 11:51:38 -0400

    state obj and html file skeleton

---
+++ app.js
@@ -0,0 +1,23 @@
+const quiz = {
+  questions: [
+    {
+      id: 0,  
+      question: "What is the capital of Maine?",
+      answer: ['Augusta', 'Bangor', 'Lake City', 'Waterville'],
+      correctAnswer: 0
+    },
+    {
+      id: 1,
+      question: "What is the capital of Wyoming?",
+      answer: ['Cody', 'Laramie', 'Cheyenne', 'Yellowstone'],
+      correctAnswer: 2
+    }
+  ],
+
+  userAnswer: [],
+  currentQuestion: null
+
+};
+
+
+
